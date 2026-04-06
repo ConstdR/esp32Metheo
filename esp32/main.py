ba63f6112490df0ddcb344b7276cb7bbff844451
ConstdR <deribin@gmail.com>
2026-04-06 13:25:45 +0200

    compact main.py

--- esp32/main.py
+++ esp32/main.py
@@ -1,57 +1,39 @@
 from espconf import *
-from machine import SoftI2C, deepsleep
+from machine import SoftI2C, Pin, ADC, deepsleep
 import mqttudp.engine as me
-import time
-import BME280
-import sht30
+import time, json, BME280, sht30
 
-from machine import Pin,PWM
+def tstamp():
+    return '%s-%.2d-%.2d %.2d:%.2d:%.2d' % time.localtime()[0:6]
 
-def run():
-    while True:
-        tstump = '%s-%.2d-%.2d %.2d:%.2d:%.2d' % time.localtime()[0:6]
-        config = { "sleep": DEEP_SLEEP, "ts_cfg": tstump}
-        try:
-            print("Publish config: %s %s" % (MY_ID, config))
-            me.send_publish('weather/%s/config' % MY_ID, json.dumps(config))
-            data = measure()
-            print("Publish data: %s " % data)
-            me.send_publish('weather/%s' % MY_ID, json.dumps(data))
-            time.sleep(1) # ? udp send async ?
-        except Exception as e:
-            print("Publish exception: %s" % e)
-        print("Deepsleep for %s sec" % str(DEEP_SLEEP/1000))
-        deepsleep(DEEP_SLEEP)
-        
+def read_adc(pin):
+    try:
+        p = ADC(Pin(pin)); p.width(p.WIDTH_12BIT); p.atten(p.ATTN_11DB)
+        return p.read_u16() / 10000
+    except Exception as e: print(e); return 0
+
 def measure():
-    tstump = '%s-%.2d-%.2d %.2d:%.2d:%.2d' % time.localtime()[0:6]
-    data = {'ts': tstump, 'v':read_adc(LVL_PIN), 'vs':read_adc(LVLSUN_PIN), 't':0, 'h':0, 'p':0}
+    data = {'ts': tstamp(), 'v': read_adc(LVL_PIN), 'vs': read_adc(LVLSUN_PIN), 't': 0, 'h': 0, 'p': 0}
     try:
         sht = sht30.SHT30(I2C_SCL, I2C_SDA)
         if sht.is_present():
-            (data['t'], data['h']) = sht.measure()
+            data['t'], data['h'] = sht.measure()
         else:
-            try:
-                i2c = SoftI2C(scl=machine.Pin(I2C_SCL), sda=machine.Pin(I2C_SDA))
-                bme = BME280.BME280(i2c=i2c)
-                (data['t'], data['h'], data['p']) = (bme.temperature, bme.humidity, bme.pressure)
-            except Exception as e:
-                data['m'] = "No measure"
-                print (data['m'])
+            bme = BME280.BME280(i2c=SoftI2C(scl=Pin(I2C_SCL), sda=Pin(I2C_SDA)))
+            data['t'], data['h'], data['p'] = bme.temperature, bme.humidity, bme.pressure
     except Exception as e:
-        print("Measure Exception")
-        print(e)
+        print(f"Measure error: {e}"); data['m'] = "No measure"
     return data
 
-def read_adc(pin):
-    res=0
-    try:
-        p=machine.ADC(machine.Pin(pin))
-        p.width(p.WIDTH_12BIT)
-        p.atten(p.ATTN_11DB)
-        res=p.read_u16()/10000
-    except Exception as e:
-        print(e)
-    return res
+def run():
+    while True:
+        try:
+            me.send_publish(f'weather/{MY_ID}/config', json.dumps({"sleep": DEEP_SLEEP, "ts_cfg": tstamp()}))
+            me.send_publish(f'weather/{MY_ID}',        json.dumps(measure()))
+            time.sleep(1)
+        except Exception as e: print(f"Publish error: {e}")
+        print(f"Deepsleep for {DEEP_SLEEP/1000}s")
+        # deepsleep(DEEP_SLEEP)
+        time.sleep(5)
 
 run()
